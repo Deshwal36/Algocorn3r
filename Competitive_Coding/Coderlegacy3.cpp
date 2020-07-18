e5fd8ff06752b06b840730ef76f8994c233b27cb
Deshwal36 <shivam.deshwal36@gmail.com>
2020-07-18 22:07:09 +0530

    Coderlegacy

---
+++ Competitive_Coding/Coderlegacy3.cpp
@@ -0,0 +1,62 @@
+/*Codechef
+  Contest : COLE2020
+  Problem : CLBRKT
+*/
+
+#include<bits\stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+     #ifndef ONLINE_JUDGE
+     freopen("in.txt","r",stdin);
+     freopen("out.txt","w",stdout);
+     #endif
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        
+        string a;
+        cin>>a;
+
+        int q;
+        cin>>q;
+        while(q--){
+            int qt;
+            cin>>qt;
+            stack<char> s;
+            while(qt!=a.size()){
+                if(s.empty() && (a[qt-1]==')')){
+                qt++;
+                }
+                if(s.empty() && (a[qt-1]=='(')){
+                    s.push(a[qt-1]);
+                    qt++;
+                }
+                if(s.top()=='(' && a[qt-1] == ')'){
+                    s.pop();
+                    if(s.empty()){
+                    cout<<qt<<"\n";
+                    break;
+                    }
+                    qt++;
+                }else{
+                    qt++;
+                }
+                
+            }
+            if(!s.empty()){
+                cout<<-1;
+            }
+        }
+    }
+
+
+    return 0 ;
+}
