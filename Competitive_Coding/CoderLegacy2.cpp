9e88225e5b0aaea544d20d30a33277474f883f04
Deshwal36 <shivam.deshwal36@gmail.com>
2020-07-26 09:08:39 +0530

    Coderlegacy : CLPNT (TLE) #2 : Resolved

--- Competitive_Coding/CoderLegacy2.cpp
+++ Competitive_Coding/CoderLegacy2.cpp
@@ -7,8 +7,9 @@
 using namespace std;
 
 int main(){
-    ios:: sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(0);
+    cout.tie(0);
 
     #ifndef ONLINE_JUDGE
     freopen("in.txt","r",stdin);
@@ -17,33 +18,36 @@ int main(){
 
     int t;
     cin>>t;
+    //cout<<"t:"<<t<<"\n";
     while(t--){
         int n;
         cin>>n;
+        //cout<<"n:"<<n<<"\n";
         int a[n];
         for(int i=0;i<n;i++){
+            //scanf("%d",a[i]);
             cin>>a[i];
         }
+        //cout<<"\n";
         
         int q;
         cin>>q;
+        //cout<<"q:"<<q<<"\n";
         while(q--){
             int x,y,cross=0;
             cin>>x>>y;
-            
-            for(int i=0;i<n;i++){
-                int sign=((x+y)-a[i]);
-                
-                if(sign==0){
-                    cross=-1;
-                    break;
-                }
-                if(sign>0)
-                    cross+=1;
-                
+            auto it=lower_bound(a,a+n,x+y);
+            int j=it-a;
+            if(j==n)
+            cout<<n<<"\n";
+            else{
+                if(a[j] == x+y)
+                cout<<"-1\n";
+                else
+                cout<<j<<"\n";
             }
-            cout<<cross<<"\n";
-           
+            //cout<<sum<<"\n";
+            //cin>>x>>y;           
         }
 
     }
