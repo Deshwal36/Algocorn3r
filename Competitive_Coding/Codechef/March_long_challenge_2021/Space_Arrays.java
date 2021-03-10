813f585638800ab56cdb4a5785d7451d0b1e7b0a
Deshwal36 <shivam.deshwal36@gmail.com>
2021-03-10 16:48:35 +0530

    Space_Array code Updated

--- Competitive_Coding/Codechef/March_long_challenge_2021/Space_Arrays.java
+++ Competitive_Coding/Codechef/March_long_challenge_2021/Space_Arrays.java
@@ -7,15 +7,12 @@ class Space_Arrays{
         test=Integer.parseInt(sc.nextLine());
         while(test>0){
             int count=0,inversion=0;
-            int n=Integer.parseInt(sc.nextLine());
+            int n=sc.nextInt();
             int[] arr=new int[n];
             for(int i=0;i<n;i++){
                 arr[i]=sc.nextInt();
             }
-            sc.nextLine();
-            if(n==1){
-                System.out.println("First");
-            }else{
+            
             for(int i=1;i<n;i++){
                 if(arr[i]<=arr[i-1]){
                     inversion=arr[i-1]-arr[i]+1;
@@ -23,9 +20,7 @@ class Space_Arrays{
                     count+=inversion;
                 }
                 
-                //System.out.println(count);
-
-            }
+           }
             
 
             if(count%2==0){
@@ -33,9 +28,10 @@ class Space_Arrays{
             }else{
                 System.out.println("First");
             }
-        }   
+      
             test--;
         }
+        sc.close();
         return ;
     }
 }
