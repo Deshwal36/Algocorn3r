928b78a38f17fe6ce661d903c725b3ae97c49ba3
Deshwal36 <shivam.deshwal36@gmail.com>
2021-03-11 09:26:29 +0530

    Codechef : March Long challenge 2021 : AC

--- Competitive_Coding/Codechef/March_long_challenge_2021/Space_Arrays.java
+++ Competitive_Coding/Codechef/March_long_challenge_2021/Space_Arrays.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 class Space_Arrays{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int test;
-        test=sc.nextInt();
+        long test;
+        test=sc.nextLong();
         while(test>0){
-            int flag=0,inversion=0;
-            int n=sc.nextInt();
-            int[] arr=new int[n];
+            long flag=0,ans=0;
+            long n=sc.nextLong();
+            long[] arr=new long[(int) n];
             
             for(int i=0;i<n;i++){
-                arr[i]=sc.nextInt();
+                arr[i]=sc.nextLong();
             }
 
             Arrays.sort(arr);
             
             for(int i=0;i<n;i++){
-                if((i+1-arr[i]) <0){
+                if(i+1<arr[i]){
                     flag=1;
                     break;
-                }
-                inversion+=(i+1-arr[i]);
+                } 
+                ans+=(i+1-arr[i]);
            }
     
             if(flag==1){
                 System.out.println("Second");
             }
             else{
-                if(inversion%2==1){
+                if(ans % 2==1){
 
                     System.out.println("First");
                 }
