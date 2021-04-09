1a44045e41f129e1c806884a3d65c5dbd49604ac
Deshwal36 <shivam.deshwal36@gmail.com>
2021-04-09 10:51:02 +0530

    HackerRank

---
+++ Competitive_Coding/Codechef/March_long_challenge_2021/_Hello_World_in_C.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * _Hello_World_in_C
+ */
+public class _Hello_World_in_C {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("Hello, World!");
+        System.out.println(s);
+    }
+}
