9bdfa1f3151e820a675ca10ba9d8f12dbe591772
Shivam Deshwal <shivam.deshwal36@gmail.com>
2021-09-16 22:56:21 +0530

    34. Find First and Last Position of Element in Sorted Array

--- Competitive_Coding/Leetcode/LeetCode_34.java
+++ Competitive_Coding/Leetcode/LeetCode_34.java
@@ -1,39 +1,41 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class LeetCode_34 {
 
     public static void main(String[] args) {
-            int[] nums={1};
-            int target=1;
+            int[] nums={2,2,2,2,2};
+            int target=3;
+
             System.out.println(Arrays.toString(searchRange(nums,target)));
     }
+    //first value >=target
+    public static int firstpos(int[] nums, int target) {
 
-    public static int[] searchRange(int[] nums, int target) {
+        int n=nums.length;
+        int low=0, high=n-1;
+        int first_pos=n;
 
-        int s=0;
-        int e=nums.length-1;
-        int[] ans={-1,-1};
-        while(s<=e){
+        while(low<=high){
+            int mid=low+(high-low)/2;
 
-            int mid=s+(e-s)/2;
-
-            if(nums[mid]==target && (mid-1<0 || nums[mid-1]<target)){
-                ans[0]=mid;
-
-                while(mid<nums.length && nums[mid]==target){
-                    mid++;
-                }
-                ans[1]=mid-1;
-                break;
-            }else {
-                if(nums[mid]>=target){
-                    e=mid-1;
-                }else{
-                    s=mid+1;
-                }
+            if(nums[mid]>=target){
+                first_pos=mid;
+                high=mid-1;
+            }else{//nums[mid]<target
+                low=mid+1;
             }
-
         }
-        return ans;
+        return first_pos;
+    }
+
+    public static int[] searchRange(int[] nums, int target){
+        int first=firstpos(nums,target);
+        int last=firstpos(nums,target+1)-1;
+        if(first<=last){
+            return new int[]{first,last};
+        }
+        return new int[]{-1,-1};
+
     }
 }
