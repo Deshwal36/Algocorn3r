d3f70bd757e3a46f51c6fdfa8aebe581c516a23d
Shivam Deshwal <shivam.deshwal36@gmail.com>
2023-08-25 11:09:48 +0530

    BFS Changes

--- Algorithm_Java/BreadthFirstSearch/App.java
+++ Algorithm_Java/BreadthFirstSearch/App.java
@@ -4,41 +4,37 @@ import java.util.List;
 
 public class App {
 
-	public static void main(String[] args) throws Exception {
-             
-            BFS bfs=new BFS();
-             
-             Vertex v1=new Vertex(5);
-             Vertex v2=new Vertex(10);
-             Vertex v3=new Vertex(25);
-             Vertex v4=new Vertex(20);
-             Vertex v5=new Vertex(15);
-             Vertex v6=new Vertex(30);
+    public static void main(String[] args) throws Exception {
 
+        BFS bfs = new BFS();
 
-             v1.setNeighbourList(List.of(v2,v3));
-             v2.setNeighbourList(List.of(v4,v6));
-             v3.setNeighbourList(List.of(v5));
+        Vertex v1 = new Vertex(5);
+        Vertex v2 = new Vertex(10);
+        Vertex v3 = new Vertex(25);
+        Vertex v4 = new Vertex(20);
+        Vertex v5 = new Vertex(15);
+        Vertex v6 = new Vertex(30);
 
+        v1.setNeighbourList(List.of(v2, v3));
+        v2.setNeighbourList(List.of(v4, v6));
+        v3.setNeighbourList(List.of(v5));
 
-             /*
-              * 
-                v1[5] {root node}  || visited:Ture , queue : 5
-                /   \
-               /      \
-            v2[10]      v3[25]
-            /   \         \
-           /     \          \
-          v4[20]  v6[30]     v5[15]
+        /*
+         * 
+         *        v1[5] {root node} || visited:Ture , queue : 5
+         *        /   \
+         *       /     \
+         *   v2[10]    v3[25]
+         *      / \       \ 
+         *     /   \        \ 
+         * v4[20]  v6[30]   v5[15]
+         * 
+         * BFS : 5->10->15->20->30->25
+         * 
+         */
 
-          BFS : 5->10->15->20->30->25
-              
-              */
-             
-             bfs.bfs(v1);
-             
-             
-             
-	}
+        bfs.bfs(v1);
+
+    }
 
 }
--- Algorithm_Java/BreadthFirstSearch/BFS.java
+++ Algorithm_Java/BreadthFirstSearch/BFS.java
@@ -4,29 +4,29 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BFS {
-	
-        public void bfs(Vertex root) {
-        	
-        	Queue<Vertex> queue = new LinkedList<>();
-        	root.setVisited(true);
-        	queue.add(root);
-        	
-        	while(!queue.isEmpty()) {
-        		
-        		Vertex actualVertex = queue.remove();
-        		System.out.println(actualVertex + " ");
-        		
-        		for(Vertex v : actualVertex.getNeighbourList()) {
-        			
-        			if(! v.isVisited()) {
-        				v.setVisited(true);
-        				queue.add(v);
-        				
-        			}
-        		}
-        		
-          }
-        	
-     }
+
+	public void bfs(Vertex root) {
+
+		Queue<Vertex> queue = new LinkedList<>();
+		root.setVisited(true);
+		queue.add(root);
+
+		while (!queue.isEmpty()) {
+
+			Vertex actualVertex = queue.remove();
+			System.out.println(actualVertex + " ");
+
+			for (Vertex v : actualVertex.getNeighbourList()) {
+
+				if (!v.isVisited()) {
+					v.setVisited(true);
+					queue.add(v);
+
+				}
+			}
+
+		}
+
+	}
 
 }
