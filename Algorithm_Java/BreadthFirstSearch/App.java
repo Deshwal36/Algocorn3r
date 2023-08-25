ed7e28c296172d847419e311a84adb38613143c5
Shivam Deshwal <shivam.deshwal36@gmail.com>
2023-08-25 11:06:10 +0530

    BFS Algo

--- Algorithm_Java/BreadthFirstSearch/App.java
+++ Algorithm_Java/BreadthFirstSearch/App.java
@@ -1,23 +1,39 @@
 package BreadthFirstSearch;
 
+import java.util.List;
+
 public class App {
 
 	public static void main(String[] args) throws Exception {
-             BFS bfs=new BFS();
+             
+            BFS bfs=new BFS();
              
              Vertex v1=new Vertex(5);
              Vertex v2=new Vertex(10);
-             Vertex v3=new Vertex(15);
+             Vertex v3=new Vertex(25);
              Vertex v4=new Vertex(20);
-             Vertex v5=new Vertex(25);
+             Vertex v5=new Vertex(15);
              Vertex v6=new Vertex(30);
-	
-             v1.addNeighbourVertex(v2);
-             v1.addNeighbourVertex(v3);
-             v2.addNeighbourVertex(v4);
-             v2.addNeighbourVertex(v6);
-             v3.addNeighbourVertex(v5);
-             //System.out.println(v1);
+
+
+             v1.setNeighbourList(List.of(v2,v3));
+             v2.setNeighbourList(List.of(v4,v6));
+             v3.setNeighbourList(List.of(v5));
+
+
+             /*
+              * 
+                v1[5] {root node}  || visited:Ture , queue : 5
+                /   \
+               /      \
+            v2[10]      v3[25]
+            /   \         \
+           /     \          \
+          v4[20]  v6[30]     v5[15]
+
+          BFS : 5->10->15->20->30->25
+              
+              */
              
              bfs.bfs(v1);
              
